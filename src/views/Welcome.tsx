146a3c95d9e4e0e20964e1eecb678ecff229ca8e
smy-101 <824860484@qq.com>
2022-08-23 16:04:04 +0800

    引入sass 准备开始写页面

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,12 +1,15 @@
 import {defineComponent} from 'vue';
 import {RouterView} from 'vue-router';
+import s from './Welcome.module.scss'
 
 export const Welcome = defineComponent({
   setup() {
     return () => (
-      <>
-        <div><RouterView/></div>
-      </>
+      <div class={s.wrapper}>
+        <header>logo</header>
+        <main><RouterView/></main>
+        <footer>button</footer>
+      </div>
     );
   },
 });
